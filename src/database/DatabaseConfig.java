e719cec62980617b3bba887914ee28b72a6bdec9
nguyenquanghuy12345678 <huynq.23ns@vku.udn.vn>
2025-11-02 10:58:36 +0700

    Update database configuration for DB2 host and credentials, and modify JDBC URL format for both databases

--- src/database/DatabaseConfig.java
+++ src/database/DatabaseConfig.java
@@ -13,19 +13,19 @@ public class DatabaseConfig {
     public static final String DB1_PASSWORD = "rmi1";
     
     // Database 2 - Máy chủ dự phòng (XAMPP trên máy này)
-    public static final String DB2_HOST = "localhost"; // Thay bằng IP máy 2
+    public static final String DB2_HOST = "192.168.56.101"; // Thay bằng IP máy 2
     public static final String DB2_PORT = "3306";
     public static final String DB2_NAME = "bank_db";
-    public static final String DB2_USER = "rmiuser1";
-    public static final String DB2_PASSWORD = "rmi1";
+    public static final String DB2_USER = "rmiuser2";
+    public static final String DB2_PASSWORD = "rmi2";
     
     public static String getDB1Url() {
-        return String.format("jdbc:mysql://%s:%s/%s?useSSL=false&serverTimezone=UTC", 
-                DB1_HOST, DB1_PORT, DB1_NAME);
+    return String.format("jdbc:mysql://%s:%s/%s?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC", 
+        DB1_HOST, DB1_PORT, DB1_NAME);
     }
     
     public static String getDB2Url() {
-        return String.format("jdbc:mysql://%s:%s/%s?useSSL=false&serverTimezone=UTC", 
-                DB2_HOST, DB2_PORT, DB2_NAME);
+    return String.format("jdbc:mysql://%s:%s/%s?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC", 
+        DB2_HOST, DB2_PORT, DB2_NAME);
     }
 }
